8b712960700debe55c0426b9bd9188abadc29229
Adil Ajjemami <adilajjemami@gmail.com>
2018-03-29 15:12:52 +0200

    manage dynamic params in errorDescription

--- test/Utils/ApiError.spec.ts
+++ test/Utils/ApiError.spec.ts
@@ -11,4 +11,12 @@ describe('ApiError', () => {
     assert.equal(apiError.getErrorDescription(), 'Page not found.');
     assert.equal(apiError.getStatus(), 404);
   });
+
+  it('constructor("invalidToken") should use GlobalMessage to get the error', () => {
+    GlobalMessage.init();
+    const apiError = new ApiError('invalidToken', { token: '----fake-token----' });
+    assert.equal(apiError.getErrorCode(), 'invalidToken');
+    assert.equal(apiError.getErrorDescription(), 'Token <----fake-token----> is invalid.');
+    assert.equal(apiError.getStatus(), 403);
+  });
 });
