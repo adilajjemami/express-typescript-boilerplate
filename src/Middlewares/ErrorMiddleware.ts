97ce5c42169f84e07cbc92e6a44ce7267d72149d
Adil Ajjemami <adilajjemami@gmail.com>
2018-03-29 16:13:49 +0200

    dynamic use of middlwares via configuration file

---
+++ src/Middlewares/ErrorMiddleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import { ApiError } from '../Utils/ApiError';
+
+/**
+ * ErrorMiddleware class.
+ *
+ * @class ErrorMiddleware
+ */
+export class ErrorMiddleware {
+  /**
+   * Handle method.
+   *
+   * @param req  Request
+   * @param res  Response
+   * @param next NextFunction
+   *
+   * @return void
+   *
+   * @static
+   */
+  public handle(err: any, req: Request, res: Response, next: NextFunction): void {
+    if (err instanceof ApiError) {
+      res.status(err.getStatus())
+        .json({
+          errorCode: err.getErrorCode(),
+          errorDescription: err.getErrorDescription(),
+        });
+    }
+    next();
+  }
+}
